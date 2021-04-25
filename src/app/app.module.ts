99e6930dceb04e24d7fcfc6c94720540175356f7
pham93 <gitloc2016@gmail.com>
2021-04-25 02:37:27 -0500

    add fade in fade out animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { AppRoutingModule } from './appRouting.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
     AppRoutingModule,
